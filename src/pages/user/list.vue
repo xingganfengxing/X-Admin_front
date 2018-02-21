56a1b0ae1eefbc54b0b23901e24084a392802c06
DESKTOP-A9HEVQA\leaf <806569552@qq.com>
2018-02-21 19:47:43 +0800

    添加用户信息列表文件

---
+++ src/pages/user/list.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script type="text/ecmascript-6">
+  const COMPONENT_NAME = 'list';
+  export default {
+    name: COMPONENT_NAME,
+    data() {
+        return {}
+    },
+    methods: {},
+    components: {}
+  }
+</script>
+
+<style scoped lang="less" type="text/less" rel="stylesheet/less">
+
+</style>
